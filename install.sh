5b6806e8bff89dcd9afaef206dfbb414c9861bd5
Maksim Shushkevich <83134047+meshushkevich@users.noreply.github.com>
2025-10-23 23:50:05 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,15 +11,15 @@ sudo cp ./pacman/pacman.conf /etc/pacman.conf
 # Update system
 sudo pacman -Suy
 
+# Install and configure Rust
+sudo pacman -S rustup
+rustup default stable
+
 # Install AUR helper (paru)
 git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
 cd ..
-
-# Install and configure Rust
-sudo pacman -S rustup
-rustup default stable
 
 # Install comtrya
 cargo install comtrya
